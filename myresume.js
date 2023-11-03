d0f971ff445b4ed393487b28bc64f7966bf6e933
Anshu Kumar <anshu.kumar2017@vitalum.ac.in>
2023-11-03 14:58:26 +0530

    write setInterval function separately

--- myresume.js
+++ myresume.js
@@ -2,6 +2,8 @@
 //Array of all Anchor tags used in nav menu
 var navMenuAnchorTags=document.querySelectorAll('.nav-menu a');
 
+//creating interval variable in global scope so scrollVertically() will know which interval to clear
+var interval; 
 
 //for loop is used for accessing all the elements of array navMenuAnchorTags
 for(var i=0;i<navMenuAnchorTags.length;i++){
@@ -15,26 +17,25 @@ for(var i=0;i<navMenuAnchorTags.length;i++){
         //To get the target section using targetSectionID
         var targetSection=document.getElementById(targetSectionID);
 
-        //To get the distance of target section from top
-        var targetSectionCoordinates=targetSection.getBoundingClientRect().top;
-
         //This will run until we reach the target section
-        var interval=setInterval(function(){
-            //To get the distance of target section from top
-            var targetSectionCoordinates=targetSection.getBoundingClientRect();
-            if(targetSectionID=="contact" && targetSectionCoordinates.top<=70){
-                clearInterval(interval);
-                return;
-            }
-            if(targetSectionCoordinates.top<=0){
-                clearInterval(interval);
-                return;
-            }
-            //Until we reach target section window will scroll by 50px after every 20 miliseconnds
-            window.scrollBy(0,50);
-
-        },15)
+        interval=setInterval(scrollVertically,15,targetSection,targetSectionID);
     })
+}
+
+function scrollVertically(targetSection,targetSectionID){
+    //To get the distance of target section from top
+    var targetSectionCoordinates=targetSection.getBoundingClientRect();
+    if(targetSectionID=="contact" && targetSectionCoordinates.top<=70){
+        clearInterval(interval);
+        return;
+    }
+    if(targetSectionCoordinates.top<=0){
+        clearInterval(interval);
+        return;
+    }
+    //Until we reach target section window will scroll by 50px after every 20 miliseconnds
+    window.scrollBy(0,50);
+
 }
 
 //Back to Top feature
